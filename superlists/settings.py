9bb542c40eea6d40c6a245f312b5c5c9a673edb6
Choumiko <ghostface@ghostface.info>
2018-02-28 19:48:33 +0100

    fix typo

--- superlists/settings.py
+++ superlists/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = os.environ['SITENAME']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
 
 else:# SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = 'en0cz94jq@#@vzq+wzr*es!y9=h6^vewi#=hj3zjsj7g(#-n^q'
