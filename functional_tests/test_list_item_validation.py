c7dd246e3c318e29497d84a203d62301c22b6508
Choumiko <ghostface@ghostface.info>
2018-03-01 01:45:40 +0100

    Moved FTs into their own individual files

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.keys import Keys
+from unittest import skip
+from .base import FunctionalTest
+
+MAX_WAIT = 10
+
+class ItemValidationTest(FunctionalTest):
+    
+    def test_cannot_add_empty_list_items(self):
+        #enter on empty input box
+        
+        #error message item cannot be blank
+        
+        #works with text
+        
+        # second blank
+        
+        #warning
+        self.fail('write me!')
+        
+        
+        
+        
